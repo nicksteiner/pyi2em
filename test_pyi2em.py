bc830e0e72d868ad0de699dfe90d09b39b1d3bdc
Nick Steiner <nick.steiner@gmail.com>
2018-09-05 14:44:33 -0400

    moved python file, fixed function

--- test_pyi2em.py
+++ test_pyi2em.py
@@ -3,7 +3,7 @@ import pyi2em
 def main():
     print("FREQ: 3.0 [GHZ], CDC: 11.3 + i1.5, CL: 10 [cm], RMS: .25 [cm], INC: 30 [deg], CORRF: Gaussian")
     e_ = pyi2em.test_I2EM()
-    print("Emissivity: %g [V], %g [H]".format(*e_))
+    print("Emissivity: {} [V], {} [H]".format(*e_))
 
 if __name__ == '__main__':
     main()

d860360ecf51f44cc1909cfeee06edc988a52698
Nick Steiner <nick.steiner@gmail.com>
2018-09-05 14:42:05 -0400

    fixed doc string

--- bld/test_pyi2em.py
+++
@@ -1,9 +0,0 @@
-import pyi2em
-
-def main()
-    print("FREQ: 3.0 [GHZ], CDC: 11.3 + i1.5, CL: 10 [cm], RMS: .25 [cm], INC: 30 [deg], CORRF: Gaussian")
-    e_ = pyi2em.test_I2EM()
-    print("Emissivity: %g [V], %g [H]".formaty(*e_))
-
-if __name__ == '__main__':
-    main()

9fd75cbdc5ec165e34c5dae8f2272f34ef693be5
Nick Steiner <nick.steiner@gmail.com>
2025-10-27 15:55:50 -0400

    chore(repo): remove build output bld/ and legacy root test; keep tests under ./tests

--- bld/pybind11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nsteiner/pyi2em_release/pybind11
-# Build directory: /home/nsteiner/pyi2em_release/bld/pybind11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
